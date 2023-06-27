d82fc015f3bf6f6fcfc4d64ff472060fe66106e0
Rin204 <0204rinsaiki@gmail.com>
2023-06-27 23:34:17 +0900

    add floor_sum

---
+++ expansion/_tests/math/floor_sum.test.py
@@ -0,0 +1,25 @@
+# verification-helper: PROBLEM https://judge.yosupo.jp/problem/sum_of_floor_of_linear
+from pathlib import Path
+import sys
+
+sys.path.append(str(Path(__file__).resolve().parent.parent.parent.parent))
+
+
+def floor_sum(n, m, a, b):
+    """
+    return \\sum_{i=0}^{n-1} ((a*i+b)//m)
+    """
+    ret = 0
+    while True:
+        if a >= m:
+            ret += (n - 1) * n // 2 * (a // m)
+            a %= m
+        if b >= m:
+            ret += n * (b // m)
+            b %= m
+        y_max = (a * n + b) // m
+        if y_max == 0:
+            return ret
+        x_max = y_max * m - b
+        ret += (n - (x_max + a - 1) // a) * y_max
+        n, m, a, b = y_max, a, m, -x_max % a
